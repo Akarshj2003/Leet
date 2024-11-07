75a92d43c9f8e93ee58acc5e9a47e7265521e087
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-11-07 08:43:09 +0530

    Time: 395 ms (92.5%), Space: 26.8 MB (81.82%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.py
@@ -1,7 +1,8 @@
 class Solution:
     def largestCombination(self, candi: List[int]) -> int:
         res=0
-        for i in range(24):
+        m=len(str(bin(max(candi))))
+        for i in range(m):
             co = sum(1 for n in candi if n & (1<<i) )
             res=max(res,co)
         return res
