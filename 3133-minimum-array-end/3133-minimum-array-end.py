cd37bba7d8ea64e342a72fe4b85f60f7507b4853
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-11-09 13:39:38 +0530

    Time: 0 ms (100%), Space: 16.7 MB (33.33%) - LeetHub

---
+++ 3133-minimum-array-end/3133-minimum-array-end.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        res = x
+        ix = 1
+        i_n = 1
+        while i_n <=n-1:
+            if ix & x == 0:
+                if i_n & (n-1):
+                    res = res | ix
+                i_n = i_n << 1
+
+            ix=ix << 1
+        return res
