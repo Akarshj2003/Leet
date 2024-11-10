1b7ad3d0e0f88b4abd662c167a4c9599fe095f3a
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-11-10 16:01:31 +0530

    Time: 1865 ms (25.58%), Space: 35.7 MB (72.93%) - LeetHub

--- 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.py
+++ 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.py
@@ -1,19 +1,29 @@
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
-        co=[]
-        for i in range(len(nums)):
-            ore=nums[i]
-            c=1
-            for j in range(i+1,len(nums)):
-                if ore >= k:
-                    break
-                ore=ore|nums[j]
-                c+=1
-            if ore >= k:    
-                co.append(c)
-        if not co:
-            return -1
-        return min(co)
+        res= float("inf")
+        bits=[0]*32
+        def bup(bit,n,to):
+            for i in range(32):
+                if n & (1 << i):
+                    bit[i] += to
+            return bit
+
+        def btoi(bit):
+            res=0
+            for i in range(32):
+                if bit[i]:
+                    res+=(1<<i)
+            return res
+        l=0
+        for r in range(len(nums)):
+            bits=bup(bits,nums[r],1)
+            ore=btoi(bits)
+            while ore>=k and l<=r:
+                res = min( res ,r-l+1)
+                bits=bup(bits,nums[l],-1)
+                ore =btoi(bits)
+                l+=1
+        return -1 if res==float("inf") else res
 
 
         
