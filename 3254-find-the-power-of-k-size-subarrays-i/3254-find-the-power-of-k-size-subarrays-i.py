81e817fdcfa73b671b468d48a553f68fec6a4431
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-11-16 13:34:26 +0530

    Time: 10 ms (40.43%), Space: 16.8 MB (21.46%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -1,18 +1,18 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
         res=[]
-        if len(nums)==1 or k==1:
-            return nums
-
-        for i in range(len(nums)-(k-1)):
-            r=-1
-            re=nums[i+(k-1)]
-            c=1
-            for i in range(i,i+(k-1)):
-                if nums[i]+1!=nums[i+1]:
-                    break
-                r=nums[i+1]
+        l=0
+        c=1
+        for r in range(len(nums)):
+            if r>0 and nums[r-1]+1==nums[r]:
                 c+=1
-            res.append(r if r == re and c==k else -1)
+            if r-l+1>k:
+                if nums[l]+1 ==nums[l+1]:
+                    c-=1
+                l+=1
+            if r-l+1 ==k:
+                if c==k:
+                    res.append(nums[r])
+                else:
+                    res.append(-1)
         return res
-        
