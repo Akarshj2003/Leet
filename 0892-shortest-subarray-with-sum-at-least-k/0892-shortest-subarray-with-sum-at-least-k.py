266957cd10763f9a4dd0c3412d41bf7042b0ee16
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-11-17 21:12:13 +0530

    Time: 225 ms (36.32%), Space: 31.8 MB (51.43%) - LeetHub

---
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        res=float("inf")
+        dq=deque()
+        csum=0
+        for  r in range(len(nums)):
+            csum+=nums[r]
+            if csum >= k:
+                res=min(res,r+1)
+            while dq and csum- dq[0][0] >= k:
+                v,e=dq.popleft()
+                res=min(res,r-e)
+            while dq and dq[-1][0]>csum:
+                dq.pop()
+            dq.append((csum,r))
+        return -1 if res==float('inf') else res 
