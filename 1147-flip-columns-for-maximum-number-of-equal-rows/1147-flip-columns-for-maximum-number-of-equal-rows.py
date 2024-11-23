5d84518b8bb847c6c871443fb8c193f3eaecbf9f
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-11-23 16:08:26 +0530

    Delete 1147-flip-columns-for-maximum-number-of-equal-rows directory

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
-        pat_freq = Counter()
-        
-        for r in matrix:
-            pattern = tuple(r) if r[0] == 0 else tuple(bit ^ 1 for bit in r)
-            pat_freq[pattern] += 1
-            
-        return max(pat_freq.values())
