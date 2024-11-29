b1f699ac86a3dfb32b9059ace3037408d32b184f
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-11-29 20:08:28 +0530

    Time: 688 ms (82.74%), Space: 30.7 MB (78.47%) - LeetHub

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.py
@@ -4,24 +4,23 @@ class Solution:
             return -1
         row,col=len(grid),len(grid[0])
         hep=[(0,0,0)]
-        visited=set()
+        visited = [[False] * col for _ in range(row)]
+        visited[0][0] = True
         naiber=((0,1),(0,-1),(1,0),(-1,0))
 
         while hep:
             t,ro,co=heappop(hep)
-            if (ro,co)==(row-1,col-1):
-                return t
-
-    
             for nr,nc in naiber:
                 r,c=ro+nr,co+nc
-                if r<0 or c<0 or r==row or c==col or (r,c) in visited:
+                if r<0 or c<0 or r>=row or c>=col or visited[r][c]:
                     continue
                 wait = 1
                 if abs(grid[r][c] - t) % 2 ==1:
                     wait=0
                 nt=max(t+1,grid[r][c]+wait)
+                if (r,c)==(row-1,col-1):
+                    return nt
                 heappush(hep,(nt,r,c))
-                visited.add((r,c))
+                visited[r][c]=True
 
         
