4d8d6601509a21da5dfe02de0df900d1591fee57
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-12-04 19:14:43 +0530

    Time: 15 ms (99.31%), Space: 17.8 MB (13.74%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
@@ -1,16 +1,25 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        n, m = len(str1), len(str2)
-        if m > n:
+        if len(str1)==len(str2):
+            for i in range(len(str1)):
+                if str2[i]=='a' and str1[i]=='z':
+                    continue
+                elif str1[i]==str2[i]:
+                    continue
+                elif ord(str2[i])-ord(str1[i])!=1:
+                    return False
+            return True
+
+        l=[]
+        if len(str2)>len(str1):
             return False
-        
-        s2 = 0  
-        for char in str1:
-            if s2 < m:
-                next_char = chr((ord(char) - ord('a') + 1) % 26 + ord('a'))
-                if char == str2[s2] or next_char == str2[s2]:
-                    s2 += 1  
-            if s2 == m:
-                return True
-        
-        return s2 == m
+        for i in str2:
+            if i not in str1:
+                l.append(i)
+        c=0
+        for i in l:
+            if chr(ord(i)-1) in str1:
+                c+=1
+            if i=='a' and 'z' in str1:
+                c+=1
+        return c==len(l)
