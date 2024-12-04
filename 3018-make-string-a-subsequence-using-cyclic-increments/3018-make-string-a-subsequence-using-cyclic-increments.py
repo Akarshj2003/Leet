aafd851f02ca581388394f6183685d1ea28e4be3
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-12-04 19:12:35 +0530

    Time: 78 ms (56.94%), Space: 17.8 MB (13.74%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
@@ -1,12 +1,16 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        n,m=len(str1),len(str2)
-        if m>n:
+        n, m = len(str1), len(str2)
+        if m > n:
             return False
-        s1,s2=0,0
-        while s1 < n and s2 < m :
-            nex=chr((ord(str1[s1])-ord('a')+1)%26 + ord('a'))
-            if(str2[s2]==str1[s1] or str2[s2]==nex ):
-                s2+=1
-            s1+=1
-        return s2==m
+        
+        s2 = 0  
+        for char in str1:
+            if s2 < m:
+                next_char = chr((ord(char) - ord('a') + 1) % 26 + ord('a'))
+                if char == str2[s2] or next_char == str2[s2]:
+                    s2 += 1  
+            if s2 == m:
+                return True
+        
+        return s2 == m
