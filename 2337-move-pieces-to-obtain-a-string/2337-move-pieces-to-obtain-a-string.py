b708cc2395df427345980fd1adad8627e251bb13
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-12-05 18:24:01 +0530

    Time: 71 ms (96.77%), Space: 17.9 MB (37.72%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.py
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.py
@@ -1,24 +1,31 @@
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
-        if start==target:
+        if start == target:
             return True
-        need_l=0
-        store_r=0
-        for s,v in zip(start, target):
-            if s=='R':
-                if need_l>0:
+        
+        l_need = 0
+        r_avail = 0
+        
+        for s, t in zip(start, target):
+            if s == 'R':
+                if l_need > 0:
                     return False
-                store_r+=1
-            if v=='L':
-                if store_r>0:
+                r_avail += 1
+            
+            if t == 'L':
+                if r_avail > 0:
                     return False
-                need_l+=1
-            if v=='R':
-                if store_r==0:
+                l_need += 1
+            
+            if t == 'R':
+                if r_avail == 0:
                     return False
-                store_r-=1
-            if s=="L":
-                if need_l==0:
+                r_avail -= 1
+            
+            if s == 'L':
+                if l_need == 0:
                     return False
-                need_l-=1                    
-        return need_l==0 and store_r==0
+                l_need -= 1
+        
+        return l_need == 0 and r_avail == 0
+
