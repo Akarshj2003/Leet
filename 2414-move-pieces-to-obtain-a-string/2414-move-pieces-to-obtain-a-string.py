231140a8106b3dd7f56d38c58511efcc3ef27bb7
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-12-05 18:21:25 +0530

    Time: 63 ms (99.19%), Space: 17.7 MB (39.52%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.py
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.py
@@ -2,20 +2,23 @@ class Solution:
     def canChange(self, start: str, target: str) -> bool:
         if start==target:
             return True
-        s_positions = [(ch, i) for i, ch in enumerate(start) if ch != '_']
-        t_positions = [(ch, i) for i, ch in enumerate(target) if ch != '_']
-        
-    
-        if len(s_positions) != len(t_positions):
-            return False
-        
-        for (s_ch, s_idx), (t_ch, t_idx) in zip(s_positions, t_positions):
-           
-            if s_ch != t_ch:
-                return False
-            if s_ch == 'L' and s_idx < t_idx:
-                return False  
-            if s_ch == 'R' and s_idx > t_idx:
-                return False  
-        
-        return True
+        need_l=0
+        store_r=0
+        for s,v in zip(start, target):
+            if s=='R':
+                if need_l>0:
+                    return False
+                store_r+=1
+            if v=='L':
+                if store_r>0:
+                    return False
+                need_l+=1
+            if v=='R':
+                if store_r==0:
+                    return False
+                store_r-=1
+            if s=="L":
+                if need_l==0:
+                    return False
+                need_l-=1                    
+        return need_l==0 and store_r==0
