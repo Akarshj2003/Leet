f25040891bad09541f386ff8f35f3776b623dea0
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2024-12-06 09:18:28 +0530

    Time: 49 ms (57.89%), Space: 19.2 MB (7.35%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.py
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -5,7 +5,7 @@ class Solution:
         banned=set(banned)
         for i in range(1,n+1):
             if cs >= maxSum or cs+i > maxSum:
-                break
+                return c
             if i not in banned: 
                 cs+=i
                 c+=1
