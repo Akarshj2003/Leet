56ad26d2e355c5a39c7e2f20ca971c6a0df3f189
Akarsh J <116100038+Akarshj2003@users.noreply.github.com>
2025-05-25 18:21:51 +0530

    Time: 167 ms (22.93%), Space: 35.6 MB (40.25%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -4,25 +4,24 @@ class Solution:
         same={}
         odds=0
         res=0
-        def is_same(word):
-            if word[0]==word[1]:
-                return True
-            else :
-                return False
         for word in words:
-            if word in same:
-                same[word]+=1
-                res+=2
-            elif is_same(word):
-                same[word]=1
-                res+=2
-            elif word[::-1] in not_same and not_same[word[::-1]] > 0:
-                not_same[word[::-1]]-=1
-                res+=4
+            if word[0]==word[1]:
+                if same.get(word,0) > 0:
+                    same[word] -= 1
+                    res+=4
+                else:
+                    same[word]=same.get(word,0)+1
             else:
-                not_same[word]=not_same.get(word,0)+1
-        odds = sum(1 for count in same.values() if count % 2 == 1)
-        if odds < 2:
-            return res
-        else:
-            return res-((odds-1)*2)
+                if not_same.get(word[::-1],0) > 0:
+                    not_same[word[::-1]] -= 1
+                    res+=4
+                else:
+                    not_same[word] = not_same.get(word,0)+1
+        for not_used_same_word_count in same.values():
+            if not_used_same_word_count > 0:
+                res+=2
+                break
+        return res
+
+
+                    
